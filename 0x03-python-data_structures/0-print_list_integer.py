c5d5d826e3dc3191272c629137e6a5d9cf7f907a
Pk-addey <pkaddey@gmail.com>
2024-09-16 23:42:58 +0000

    Modify code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,5 @@
 
 
 def print_list_integer(my_list=[]):
-    """Print all integers in a list, one per line."""
-    for number in my_list:
-        print("{}".format(number))
+    for i in my_list:
+        print("{:d}".format(i))
