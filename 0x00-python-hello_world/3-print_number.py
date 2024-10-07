78e7103bb6573c5bf51b3d50a22c8631dca9076f
Pk-addey <pkaddey@gmail.com>
2024-10-07 05:34:40 +0000

    Updated code to comply with pycodestyle.

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 number = 98
-try: print(f"{number} Battery street")
-except: print(f"Error: {type(number).__name__} type is not supported")
+try:
+    print(f"{number} Battery street")
+except TypeError:
+    print(f"Error: {type(number).__name__} type is not supported")
