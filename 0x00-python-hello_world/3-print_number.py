c85dae6f088f544cd03bd74b88ae99ddc7faaf59
Pk-addey <pkaddey@gmail.com>
2024-10-07 13:33:08 +0000

    Updated code.

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-number = input()
-if number.isdigit() or (number[0] == '-' and number[1:].isdigit()):
-    print(f"{int(number)} Battery street")
-else:
-    raise ValueError("Unknown format code 'd' for object of type 'str'")
+number = 98
+print(f"{number} Battery street")
