6a795f0ffdf127759dd44d95161c63d9b27f0470
Pk-addey <pkaddey@gmail.com>
2024-10-07 05:47:48 +0000

    Modify code.

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-number = 98
-try:
-    print(f"{number} Battery street")
-except TypeError:
-    print(f"Error: {type(number).__name__} type is not supported")
+number = -98
+print(f"{number} Battery street")
