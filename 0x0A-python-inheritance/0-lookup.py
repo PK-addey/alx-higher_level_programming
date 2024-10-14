99c051333b63446047a219731380ec8a9c136999
Pk-addey <pkaddey@gmail.com>
2024-10-14 08:11:09 +0000

    Implement lookup function to return list of attributes and methods of an object.

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+
+    :param obj: The object to inspect
+    :return: List of attributes and methods of the object
+    """
+    return dir(obj)
