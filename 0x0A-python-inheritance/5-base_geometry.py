75399a3749a90f4c33e214b3905070350c8009d3
Pk-addey <pkaddey@gmail.com>
2024-10-14 08:54:11 +0000

    Create BaseGeometry class as a foundation for geometric shapes.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGeometry class that serves as a foundation for geometric shapes.
+    """
+    pass
