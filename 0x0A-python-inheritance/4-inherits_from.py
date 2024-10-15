72dab410718870ae2033a012e3a1ae204ef7c516
Pk-addey <pkaddey@gmail.com>
2024-10-15 11:46:31 +0000

    Fix PEP 8 issue: Use 'is not' for exact type checks in is_same_class method.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,4 +13,4 @@ def inherits_from(obj, a_class):
     Returns:
         True if obj is a true subclass of a_class, otherwise False.
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
